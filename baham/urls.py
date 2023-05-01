79004947771fd3be3a8b5d5fb8c7adb26d9b60b0
falakn2405 <falakn2405@gmail.com>
2023-05-01 13:31:43 +0500

    Update urls.py

--- baham/urls.py
+++ baham/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
       path('', views.view_home, name='home'),
-      path('baham/aboutus', views.view_aboutus, name='aboutus')
+      path('baham/aboutus', views.view_aboutus, name='aboutus'),
+      path('baham/header', views.view_header, name='header'),
+      path('bahm.fooetr', views.view_footer, name='footer')
 ]
