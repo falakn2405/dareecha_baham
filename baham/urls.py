bad94596a45634d1e5fbff04e4c507ba2a0a0a7a
falakn2405 <falakn2405@gmail.com>
2023-05-01 13:18:05 +0500

    Baham-App

--- baham/urls.py
+++ baham/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('', views.home, name='home')
+      path('', views.view_home, name='home'),
+      path('baham/aboutus', views.view_aboutus, name='aboutus')
 ]
--- baham/views.py
+++ baham/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.template import loader
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1> Welcome to Baham </h1>")
+def view_home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({}, request))
+
+# Create your views here.
+def view_aboutus(request):
+    template = loader.get_template('aboutus.html')
+    return HttpResponse(template.render({}, request))
