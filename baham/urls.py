3f7b885c788157db22abda5aff1aba346cd7b038
falakn2405 <falakn2405@gmail.com>
2023-05-03 15:05:15 +0500

    Update urls.py

--- baham/urls.py
+++ baham/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
       path('', views.view_home, name='home'),
+      path('baham/vehicles/', views.view_vehicles, name='vehicles'),
+      path('baham/vehicles/create/', views.create_vehicle, name='create_vehicle'),
+      
       path('baham/aboutus', views.view_aboutus, name='aboutus'),
 ]
