3fc95be0061d6cd2cb6a0eada2835afb5145a7e8
falakn2405 <falakn2405@gmail.com>
2023-05-17 21:10:01 +0500

    Update models.py

--- baham/models.py
+++ baham/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from baham.constants import COLOURS, TOWNS
-from baham.enum_types import UserType, VehicleType, VehicleStatus
+from baham.enum_types import VehicleType, VehicleStatus, UserType
 
 
 # Custom validators
@@ -15,40 +15,8 @@ def validate_colour(value):
 
 
 # Create your models here.
-class VehicleModel(models.Model):
-    model_id = models.AutoField(primary_key=True, db_column='id')
-    vendor = models.CharField(max_length=20, null=False, blank=False)
-    model = models.CharField(max_length=20, null=False, blank=False, default='Unknown')
-    type = models.CharField(max_length=50, choices=[(t.name, t.value) for t in VehicleType],
-                            help_text="Select the vehicle chassis type")
-    capacity = models.PositiveSmallIntegerField(null=False, default=2)
-
-    class Meta:
-        db_table = "baham_vehicle_model"
-
-    def __str__(self):
-        return f"{self.vendor} {self.model}"
-
-
-class Vehicle(models.Model):
-    vehicle_id = models.AutoField(primary_key=True, db_column='id')
-    registration_number = models.CharField(max_length=10, unique=True, null=False, blank=False,
-                                           help_text="Unique registration/license plate no. of the vehicle.")
-    colour = models.CharField(max_length=50, default='white', validators=[validate_colour])
-    model = models.ForeignKey(VehicleModel, null=False, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    date_added = models.DateField(default=now, editable=False)
-    status = models.CharField(max_length=50, choices=[(t.name, t.value) for t in VehicleStatus])
-    picture1 = models.ImageField(upload_to='pictures', null=True)
-    picture2 = models.ImageField(upload_to='pictures', null=True)
-
-    def __str__(self):
-        return f"{self.model.vendor} {self.model.model} {self.colour}"
-
-
 class UserProfile(models.Model):
-    # Should have one-to-one relationship with a Django user
-
+    # Should have one-to-one relationship with Django user
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthdate = models.DateField()
     gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
@@ -60,14 +28,48 @@ class UserProfile(models.Model):
     address_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     landmark = models.CharField(max_length=255, null=False)
     town = models.CharField(max_length=50, null=False, choices=[(c, c) for c in TOWNS])
-    date_created = models.DateField(auto_now_add=True)
     active = models.BooleanField(default=True, editable=False)
     date_deactivated = models.DateTimeField(editable=False, null=True)
     bio = models.TextField()
 
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name}"
-    
+        return f"{self.username} {self.first_name} {self.last_name}"
+
+
+class VehicleModel(models.Model):
+    model_id = models.AutoField(primary_key=True, db_column='id')
+    # Toyota, Honda, Suzuki, Kia, etc.
+    vendor = models.CharField(max_length=20, null=False, blank=False)
+    # Corolla, Vitz, City, Sportage, etc.
+    model = models.CharField(max_length=20, null=False, blank=False, default='Unknown')
+    # Sedan, Motorcyle, SUV, Van, etc.
+    type = models.CharField(max_length=50, choices=[(t.name, t.value) for t in VehicleType],
+                            help_text="Select the vehicle chassis type")
+    # Sitting capacity
+    capacity = models.PositiveSmallIntegerField(null=False, default=2)
+
+    class Meta:
+        db_table = "baham_vehicle_model"
+
+    def __str__(self):
+        return f"{self.vendor} {self.model}"
+
+
+class Vehicle(models.Model):
+    vehicle_id = models.AutoField(primary_key=True, db_column='id')
+    # ABC-877
+    registration_number = models.CharField(max_length=10, unique=True, null=False, blank=False,
+                                           help_text="Unique registration/license plate no. of the vehicle.")
+    colour = models.CharField(max_length=50, default='white', validators=[validate_colour])
+    model = models.ForeignKey(VehicleModel, null=False, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=[(t.name, t.value) for t in VehicleStatus])
+    picture1 = models.ImageField(upload_to='pictures', null=True)
+    picture2 = models.ImageField(upload_to='pictures', null=True)
+
+    def __str__(self):
+        return f"{self.model.vendor} {self.model.model} {self.colour}"
+
 
 class Contract(models.Model):
     contract_id = models.AutoField(primary_key=True, db_column='id')
@@ -77,5 +79,5 @@ class Contract(models.Model):
     expiry_date = models.DateField()
     is_active = models.BooleanField(default=True)
     fuel_share = models.PositiveSmallIntegerField(help_text="Percentage of fuel contribution.")
-    maintenance_share = models.PositiveSmallIntegerField(help_text="Percentage of mantenance cost contribution.")
-    schedule = models.CharField(max_length=255, null=False)
+    maintenance_share = models.PositiveSmallIntegerField(help_text="Percentage of maintenance cost contribution.")
+    schedule = models.CharField(max_length=255, null=False)  #TODO: use Django Scheduler
